7d0ad50bc9e0f384c8a22e3bc90df124f4e028f1
Bilqis <hirosasi01@gmail.com>
2021-08-05 11:31:08 +0700

    update 1

--- android/app/src/main/kotlin/com/muflihunnisa/trial_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/muflihunnisa/trial_flutter/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.muflihunnisa.trial_flutter
 
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
+//import io.flutter.embedding.android.FlutterActivity
+//
+//class MainActivity: FlutterActivity() {
+//}
---
+++ lib/login_screen.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool passwordVisibility = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 4.0,
+        leading: Padding(
+          padding: EdgeInsets.all(12),
+          child: Image.network(
+              'https://d17ivq9b7rppb3.cloudfront.net/original/commons/new-ui-logo.png'),
+        ),
+      ),
+      endDrawer: Drawer(
+        child: SafeArea(
+          child: Column(
+            children: <Widget>[
+              MenuTile(title: 'Event'),
+              MenuTile(title: 'Courses'),
+              MenuTile(title: 'Academy'),
+              MenuTile(title: 'Challenge')
+            ],
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(vertical: 32, horizontal: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Image.asset('assets/img/dicoding_icon.png',
+                width: 200, height: 100),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Masuk',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold),
+            ),
+
+            /// Option
+            /// Theme.of(context).textTheme.headline3!.copyWith(color: Colors.black, fontWeight: FontWeight.bold),)
+            Center(child: Image.asset('assets/img/ilustration.png')),
+            SizedBox(
+              height: 16,
+            ),
+            Text(
+              'Email',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                  hintText: 'Alamat Email',
+                  border: OutlineInputBorder(),
+                  focusedBorder: OutlineInputBorder(),
+                  isDense: true),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Text(
+              'Password',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            TextField(
+              obscureText: passwordVisibility,
+              decoration: InputDecoration(
+                  hintText: 'Masukkan Password',
+                  border: OutlineInputBorder(),
+                  focusedBorder: OutlineInputBorder(),
+                  suffixIcon: IconButton(
+                    icon: Icon(passwordVisibility
+                        ? Icons.visibility
+                        : Icons.visibility_off),
+                    onPressed: () {
+                      setState(() {
+                        passwordVisibility = !passwordVisibility;
+                      });
+                    },
+                  ),
+                  isDense: true),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Text(
+              'Lupa Password?',
+              style:
+                  TextStyle(fontSize: 16, decoration: TextDecoration.underline),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(primary: Colors.pink),
+              onPressed: () {
+                //jika dilempar ke halaman lain
+              },
+              child: Text(
+                'Masuk',
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Text('Atau masuk dengan', style: TextStyle(fontSize: 16),
+            ),
+            Row(
+              children: <Widget>[
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: Colors.white),
+                    onPressed: (){},
+                    child: Image.network('https://pngimg.com/uploads/google/google_PNG19644.png',
+                      width: 55,)
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(primary: Colors.white),
+                    onPressed: (){},
+                    child: Image.network('https://pngimg.com/uploads/facebook_logos/facebook_logos_PNG19749.png',
+                      width: 55,)
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MenuTile extends StatelessWidget {
+  final String title;
+
+  const MenuTile({required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+        title: Text(title, style: Theme.of(context).textTheme.bodyText1),
+        trailing: Icon(Icons.navigate_next, color: Colors.black));
+  }
+}
